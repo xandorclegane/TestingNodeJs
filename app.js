b610355ea92e5044c046647850b207e72513d861
xandor_clegane <juan.cardenas3@utp.edu.co>
2024-02-16 05:26:51 +0000

    hola mundo

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('¡Hola mundo desde Express!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
+
